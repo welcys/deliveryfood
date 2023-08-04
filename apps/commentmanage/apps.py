f5d70c907f747c86366e4028e1cbfbf93275d4be
welch <welcys@github.com>
2023-08-04 17:58:00 +0800

    第一次提交

---
+++ apps/commentmanage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MessmanageConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.commentmanage"
---
+++ deliveryfood/settings/develop.py
@@ -0,0 +1,25 @@
+from .base import *
+
+DEBUG = True
+
+
+DATABASES = {
+    'default': {'ENGINE': 'django.db.backends.mysql',
+                'NAME': 'deliveryfood',
+                'USER': 'root',
+                'PASSWORD': 'Asdf1234.',
+                'HOST': '127.0.0.1',
+                'PORT': '3306',
+                }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'PARSER_CLASS': 'redis.connection.HiredisParser',
+        },
+    },
+}
